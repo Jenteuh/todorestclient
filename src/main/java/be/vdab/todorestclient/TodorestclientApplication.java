4d487e8081367a795432dbb61188dfc5f18be761
Jente Huysmans <jentehuysmans@hotmail.com>
2025-09-16 15:07:34 +0200

    Initial commit

---
+++ src/main/java/be/vdab/todorestclient/TodorestclientApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.todorestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodorestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodorestclientApplication.class, args);
+    }
+
+}
